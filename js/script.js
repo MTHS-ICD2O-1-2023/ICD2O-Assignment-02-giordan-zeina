94b169d939da43fceb331049f4a3119c33143205
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-03-27 17:18:36 +0000

    favi icon & code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Giordan Zeina
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates Perimeter of a rectangle.
+ */
+function calculatePerimeterOfRectangle () {
+  // input
+  const lengthOfRectangle = parseInt(document.getElementById('length-of-rectangle').value)
+  const widthOfRectangle = parseInt(document.getElementById('width-of-rectangle').value)
+
+  // process
+  const calculatePerimeterOfRectangle = 2 * (lengthOfRectangle * widthOfRectangle)
+
+  // output
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: '+ calculatePerimeterOfRectangle +' cm²'
+}
